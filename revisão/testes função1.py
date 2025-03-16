cf807cb1096ae55fd346165088dd22dee8dc5c5e
Ivaldonetto <ivaldoneto42@gmail.com>
2025-03-15 23:52:23 -0300

    att

---
+++ revisão/testes função1.py
@@ -0,0 +1,49 @@
+# def mostrarlinhas():
+#     print('=-'*30)
+#
+#
+# mostrarlinhas()
+# print('TESTANDO GAMES')
+# mostrarlinhas()
+# print('COMENDO PÃO FRANCÊS')
+# mostrarlinhas()
+# print('FALANDO COM PESSOAS GRANDES')
+# mostrarlinhas()
+# print('TENHO UM AMIGO CHAMADO PIMER')
+# mostrarlinhas()
+
+# def mensagem(msg):
+#     print('=-'*20)
+#     print(msg)
+#     print('=-'*20)
+#
+# mensagem('CARNE DE CABRITO')
+# mensagem('teste som')
+
+# def soma(a,b):
+#     s = a+b
+#     print(f'{a} + {b} = {s}')
+#
+#
+# soma(4,5)
+# soma(b=8,a=9) #você pode escolher qual vai ser o A ou o B
+# soma(2,1)
+
+# def contador(* núm): #o * faz você digitar quantos parametros quiser na função.
+#     print(núm) #dessa forma o valor vai virar uma tupla.
+#     tam = len(núm)
+#     print(f'existe {tam} números na tupla.')
+#
+# contador(1,2,3)
+# contador(5,9)
+# contador(2,7)
+
+# def dobra(lst): #você pode usar a lista
+#     p = 0
+#     while p < len(lst):
+#         lst[p] = lst[p] * 2
+#         p += 1
+#
+# valores = [1,5,3,0,7,10]
+# dobra(valores)
+# print(valores)
