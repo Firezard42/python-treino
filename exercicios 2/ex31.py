cdcb94b28c95e5f3a97f5c5cfcb5e13e58455eb5
Ivaldo Neto <ivaldoneto42@gmail.com>
2025-10-29 17:09:44 -0300

    Create ex31.py

---
+++ exercicios 2/ex31.py
@@ -0,0 +1,24 @@
+#Create a Python program that takes a sequence of lines as input and returns each line capitalized.
+
+
+
+lista1 = str(input('Escreva palavras separadas por vírgulas : ')).upper().split(',')
+
+print(lista1)
+
+
+#--------------------------------------------------------------------------
+
+#capitalize = ['Sundays Are Fun','Monday is done']
+
+#for c in capitalize:
+    #c = c.upper()
+    #print(c)
+
+#-----------------------------------------------------------------------
+
+#def capitalize_lines(lines):
+    #return [line.upper() for line in lines]
+
+# Teste
+#print(capitalize_lines(['Sundays are Fun', 'Monday is Done']))
